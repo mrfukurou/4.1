6b420fa3b220d920058d9bc344812221e9afcb65
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-12 22:26:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static double a(int n)
+        {
+            if (n == 1) return 1;
+
+            else return (a(n / 2) + 1);
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите n: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n > 0)
+                { Console.Write("a = " + a(n)); }
+                else Console.Write("Число должно быть больше 0!");
+            }
+            catch { Console.Write("Некорректный ввод!"); }
+
+        }
+    }
+}
